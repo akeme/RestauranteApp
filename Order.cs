b1fe5954d593d1285d3a9de10f7376cb06a4fd00
Mariana Ogawa <mari.ogawa@gmail.com>
2022-02-02 16:55:39 -0300

    Adicionar arquivos de projeto.

---
+++ Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestauranteApp
+{
+    internal class Order
+    {
+        public int Items { get; set; }
+        
+        public int QtdItems { get; set; }
+    }
+}
